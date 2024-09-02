5de51bc2e80513abd7d59295e72667da8c4df6c5
MohamedMohamed <m01126165016@icloud.com>
2024-09-02 21:32:55 +0300

    ADD Fonts And icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                inter: ["Inter"]
+            },
+            colors: {
+                primary: "#F2F2F2",
+                secondary: "#4EA8DE",
+                background: "#1A1A1A",
+            }
+        },
     },
     plugins: [],
 }
